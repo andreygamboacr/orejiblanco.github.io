c07b288bf1152e0beaeb39bd6a9b03653ace531a
andreygamboa <gamboa.andrey@gmail.com>
2026-09-20 13:38:36 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const translations = {
+    es: {
+        heroTagline: "Café que nace de la tierra que cuidamos.",
+        scrollButton: "CONOZCA NUESTRA FINCA",
+        historia: "Nuestra historia"
+    },
+
+    en: {
+        heroTagline: "Coffee born from the land we care for.",
+        scrollButton: "DISCOVER OUR FARM",
+        historia: "Our story"
+    }
+};
